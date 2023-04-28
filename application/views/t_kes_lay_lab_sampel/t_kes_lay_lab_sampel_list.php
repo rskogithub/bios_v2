b3b02e60947e998df07acf4beaad515934ac198e
khotamawb <113576380+rskogithub@users.noreply.github.com>
2023-04-28 15:17:52 +0700

    lab sampel

---
+++ application/views/t_kes_lay_lab_sampel/t_kes_lay_lab_sampel_list.php
@@ -0,0 +1,67 @@
+<main id="js-page-content" role="main" class="page-content">
+    <?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
+    <div class="row">
+        <div class="col-xl-12">
+            <div id="panel-1" class="panel">
+                <div class="panel-hdr">
+                    <h2>INPUT DATA LAYANAN LABORATORIUM (SAMPEL)</h2>
+                    <div class="panel-toolbar">
+                        <button class="btn btn-panel" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
+                        <button class="btn btn-panel" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
+                        <button class="btn btn-panel" data-action="panel-close" data-toggle="tooltip" data-offset="0,10" data-original-title="Close"></button>
+                    </div>
+                </div>
+                <div class="panel-container show">
+                    <div class="panel-content">
+                        <div class="text-center">
+                            <?php echo anchor(site_url('t_kes_lay_lab_sampel/create'), '<i class="fal fa-plus-square" aria-hidden="true"></i> Tambah Data', 'class="btn btn-primary btn-sm waves-effect waves-themed"'); ?></div>
+                        <table class="table table-bordered table-hover table-striped w-100" id="dt-basic-example">
+
+                            <thead>
+                                <tr>
+                                    <th width="30px">No</th>
+                                    <th>Tgl Transaksi</th>
+                                    <th>Jumlah</th>
+                                    <th>Nama Satker</th>
+                                    <th>Create Date</th>
+                                    <!-- <th width="200px">Action</th> -->
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $start = 0;
+                                foreach ($data as $row) {
+                                ?>
+                                    <tr>
+                                        <td width="2%"><?php echo $row['rn'] ?></td>
+                                        <td><?php echo $row['tgl_transaksi'] ?></td>
+                                        <td><?php echo $row['jumlah'] ?></td>
+                                        <td><?php echo $row['nmsatker'] ?></td>
+                                        <td><?php echo $row['updated_at'] ?></td>
+                                    </tr>
+                                <?php
+                                }
+                                ?>
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<script src="<?php echo base_url() ?>assets/smartadmin/js/vendors.bundle.js"></script>
+<script src="<?php echo base_url() ?>assets/smartadmin/js/app.bundle.js"></script>
+<script src="<?php echo base_url() ?>assets/smartadmin/js/datagrid/datatables/datatables.bundle.js"></script>
+<script src="<?php echo base_url() ?>assets/smartadmin/js/datagrid/datatables/datatables.export.js"></script>
+<script src="<?php echo base_url() ?>assets/smartadmin/js/notifications/toastr/toastr.js"></script>
+<script>
+    $(document).ready(function() {
+
+        var table = $('#dt-basic-example').DataTable({
+            //responsive: true,
+            orderCellsTop: true,
+            fixedHeader: true,
+        });
+    });
+</script>
