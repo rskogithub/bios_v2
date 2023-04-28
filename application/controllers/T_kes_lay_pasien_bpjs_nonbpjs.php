a24775b18b36f446bd5399859e1a51abd4ed88f9
khotamawb <113576380+rskogithub@users.noreply.github.com>
2023-04-28 15:31:31 +0700

    update lay bpjs n nonbpjs

--- application/controllers/T_kes_lay_pasien_bpjs_nonbpjs.php
+++ application/controllers/T_kes_lay_pasien_bpjs_nonbpjs.php
@@ -3,13 +3,13 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class T_kes_lay_lab_sampel extends CI_Controller
+class T_kes_lay_pasien_bpjs_nonbpjs extends CI_Controller
 {
     function __construct()
     {
         parent::__construct();
         is_login();
-        $this->load->model('T_kes_lay_lab_sampel_model');
+        $this->load->model('T_kes_lay_pasien_bpjs_nonbpjs_model');
         $this->load->library('form_validation');
         $this->load->library('datatables');
     }
@@ -17,33 +17,34 @@ class T_kes_lay_lab_sampel extends CI_Controller
     public function index()
     {
         $data = array(
-            'data' => $this->T_kes_lay_lab_sampel_model->get_all(),
+            'data' => $this->T_kes_lay_pasien_bpjs_nonbpjs_model->get_all(),
         );
-        $this->template->load('template', 't_kes_lay_lab_sampel/t_kes_lay_lab_sampel_list', $data);
+        $this->template->load('template', 't_kes_lay_pasien_bpjs_nonbpjs/t_kes_lay_pasien_bpjs_nonbpjs_list', $data);
     }
 
     public function json()
     {
         header('Content-Type: application/json');
-        echo $this->T_kes_lay_lab_sampel_model->json();
+        echo $this->T_kes_lay_pasien_bpjs_nonbpjs_model->json();
     }
 
     public function read($id)
     {
-        $row = $this->T_kes_lay_lab_sampel_model->get_by_id($id);
+        $row = $this->T_kes_lay_pasien_bpjs_nonbpjs_model->get_by_id($id);
         if ($row) {
             $data = array(
                 'id' => $row->id,
                 'tgl_transaksi' => $row->tgl_transaksi,
-                'jumlah' => $row->jumlah,
+                'jumlah_bpjs' => $row->jumlah_bpjs,
+                'jumlah_non_bpjs' => $row->jumlah_non_bpjs,
                 'message' => $row->message,
                 'user' => $row->user,
                 'create_date' => $row->create_date,
             );
-            $this->template->load('template', 't_kes_lay_lab_sampel/t_kes_lay_lab_sampel_read', $data);
+            $this->template->load('template', 't_kes_lay_pasien_bpjs_nonbpjs/t_kes_lay_pasien_bpjs_nonbpjs_read', $data);
         } else {
             $this->session->set_flashdata('warning', 'Record Not Found');
-            redirect(site_url('t_kes_lay_lab_sampel'));
+            redirect(site_url('t_kes_lay_pasien_bpjs_nonbpjs'));
         }
     }
 
@@ -51,15 +52,16 @@ class T_kes_lay_lab_sampel extends CI_Controller
     {
         $data = array(
             'button' => 'Kirim',
-            'action' => site_url('t_kes_lay_lab_sampel/create_action'),
+            'action' => site_url('t_kes_lay_pasien_bpjs_nonbpjs/create_action'),
             'id' => set_value('id'),
             'tgl_transaksi' => set_value('tgl_transaksi'),
-            'jumlah' => set_value('jumlah'),
+            'jumlah_bpjs' => set_value('jumlah_bpjs'),
+            'jumlah_non_bpjs' => set_value('jumlah_non_bpjs'),
             'message' => set_value('message'),
             'user' => set_value('user'),
             'create_date' => set_value('create_date'),
         );
-        $this->template->load('template', 't_kes_lay_lab_sampel/t_kes_lay_lab_sampel_form', $data);
+        $this->template->load('template', 't_kes_lay_pasien_bpjs_nonbpjs/t_kes_lay_pasien_bpjs_nonbpjs_form', $data);
     }
 
     public function create_action()
@@ -67,36 +69,37 @@ class T_kes_lay_lab_sampel extends CI_Controller
         $tanggal = date('Y-m-d', strtotime($this->input->post('tgl_transaksi', TRUE)));
         $data = array(
             'tgl_transaksi' => $tanggal,
-            'nama_layanan' => $this->input->post('nama_layanan', TRUE),
-            'jumlah' => $this->input->post('jumlah', TRUE),
+            'jumlah_bpjs' => $this->input->post('jumlah_bpjs', TRUE),
+            'jumlah_non_bpjs' => $this->input->post('jumlah_non_bpjs', TRUE),
         );
-        $row = $this->T_kes_lay_lab_sampel_model->get_by_param($tanggal);
+        $row = $this->T_kes_lay_pasien_bpjs_nonbpjs_model->get_by_param($tanggal);
         if (($tanggal == $row->tgl_transaksi)) {
-            $this->T_kes_lay_lab_sampel_model->update_kes_lay_lab_sampel($data);
+            $this->T_kes_lay_pasien_bpjs_nonbpjs_model->update_kes_lay_pasien_bpjs_nonbpjs($data);
         } else {
-            $this->T_kes_lay_lab_sampel_model->insert_kes_lay_lab_sampel($data);
+            $this->T_kes_lay_pasien_bpjs_nonbpjs_model->insert_kes_lay_pasien_bpjs_nonbpjs($data);
         }
     }
 
     public function update($id)
     {
-        $row = $this->T_kes_lay_lab_sampel_model->get_by_id($id);
+        $row = $this->T_kes_lay_pasien_bpjs_nonbpjs_model->get_by_id($id);
 
         if ($row) {
             $data = array(
                 'button' => 'Update',
-                'action' => site_url('t_kes_lay_lab_sampel/update_action'),
+                'action' => site_url('t_kes_lay_pasien_bpjs_nonbpjs/update_action'),
                 'id' => set_value('id', $row->id),
                 'tgl_transaksi' => set_value('tgl_transaksi', $row->tgl_transaksi),
-                'jumlah' => set_value('jumlah', $row->jumlah),
+                'jumlah_bpjs' => set_value('jumlah_bpjs', $row->jumlah_bpjs),
+                'jumlah_non_bpjs' => set_value('jumlah_non_bpjs', $row->jumlah_non_bpjs),
                 'message' => set_value('message', $row->message),
                 'user' => set_value('user', $row->user),
                 'create_date' => set_value('create_date', $row->create_date),
             );
-            $this->template->load('template', 't_kes_lay_lab_sampel/t_kes_lay_lab_sampel_form', $data);
+            $this->template->load('template', 't_kes_lay_pasien_bpjs_nonbpjs/t_kes_lay_pasien_bpjs_nonbpjs_form', $data);
         } else {
             $this->session->set_flashdata('warning', 'Record Not Found');
-            redirect(site_url('t_kes_lay_lab_sampel'));
+            redirect(site_url('t_kes_lay_pasien_bpjs_nonbpjs'));
         }
     }
 
@@ -109,29 +112,30 @@ class T_kes_lay_lab_sampel extends CI_Controller
         // } else {
         $data = array(
             'tgl_transaksi' => $this->input->post('tgl_transaksi', TRUE),
-            'jumlah' => $this->input->post('jumlah', TRUE),
+            'jumlah_bpjs' => $this->input->post('jumlah_bpjs', TRUE),
+            'jumlah_non_bpjs' => $this->input->post('jumlah_non_bpjs', TRUE),
             'message' => $this->input->post('message', TRUE),
             'user' => $this->input->post('user', TRUE),
             'create_date' => $this->input->post('create_date', TRUE),
         );
 
-        $this->T_kes_lay_lab_sampel_model->update($this->input->post('id', TRUE), $data);
+        $this->T_kes_lay_pasien_bpjs_nonbpjs_model->update($this->input->post('id', TRUE), $data);
         $this->session->set_flashdata('success', ' Update Record Success');
-        redirect(site_url('t_kes_lay_lab_sampel'));
+        redirect(site_url('t_kes_lay_pasien_bpjs_nonbpjs'));
         // }
     }
 
     public function delete($id)
     {
-        $row = $this->T_kes_lay_lab_sampel_model->get_by_id($id);
+        $row = $this->T_kes_lay_pasien_bpjs_nonbpjs_model->get_by_id($id);
 
         if ($row) {
-            $this->T_kes_lay_lab_sampel_model->delete($id);
+            $this->T_kes_lay_pasien_bpjs_nonbpjs_model->delete($id);
             $this->session->set_flashdata('success', ' Delete Record Success');
-            redirect(site_url('t_kes_lay_lab_sampel'));
+            redirect(site_url('t_kes_lay_pasien_bpjs_nonbpjs'));
         } else {
             $this->session->set_flashdata('warning', 'Record Not Found');
-            redirect(site_url('t_kes_lay_lab_sampel'));
+            redirect(site_url('t_kes_lay_pasien_bpjs_nonbpjs'));
         }
     }
 
@@ -140,4 +144,4 @@ class T_kes_lay_lab_sampel extends CI_Controller
 
 }
 
-/* End of file T_kes_lay_lab_sampel.php */
+/* End of file T_kes_lay_pasien_bpjs_nonbpjs.php */
--- application/views/t_kes_lay_pasien_bpjs_nonbpjs/t_kes_lay_pasien_bpjs_nonbpjs_list.php
+++ application/views/t_kes_lay_pasien_bpjs_nonbpjs/t_kes_lay_pasien_bpjs_nonbpjs_list.php
@@ -1,10 +1,9 @@
 <main id="js-page-content" role="main" class="page-content">
-    <?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
     <div class="row">
         <div class="col-xl-12">
             <div id="panel-1" class="panel">
                 <div class="panel-hdr">
-                    <h2>KELOLA DATA LAYANAN LABORATORIUM (SAMPEL)</h2>
+                    <h2>KELOLA DATA LAYANAN PASIEN BPJS DAN NON BPJS</h2>
                     <div class="panel-toolbar">
                         <button class="btn btn-panel" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
                         <button class="btn btn-panel" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
@@ -14,14 +13,14 @@
                 <div class="panel-container show">
                     <div class="panel-content">
                         <div class="text-center">
-                            <?php echo anchor(site_url('t_kes_lay_lab_sampel/create'), '<i class="fal fa-plus-square" aria-hidden="true"></i> Tambah Data', 'class="btn btn-primary btn-sm waves-effect waves-themed"'); ?></div>
+                            <?php echo anchor(site_url('t_kes_lay_pasien_bpjs_nonbpjs/create'), '<i class="fal fa-plus-square" aria-hidden="true"></i> Tambah Data', 'class="btn btn-primary btn-sm waves-effect waves-themed"'); ?></div>
                         <table class="table table-bordered table-hover table-striped w-100" id="dt-basic-example">
-
                             <thead>
                                 <tr>
                                     <th width="30px">No</th>
                                     <th>Tgl Transaksi</th>
-                                    <th>Jumlah</th>
+                                    <th>Jumlah BPJS</th>
+                                    <th>Jumlah NON BPJS</th>
                                     <th>Nama Satker</th>
                                     <th>Create Date</th>
                                     <!-- <th width="200px">Action</th> -->
@@ -34,7 +33,8 @@
                                     <tr>
                                         <td width="2%"><?php echo $row['rn'] ?></td>
                                         <td><?php echo $row['tgl_transaksi'] ?></td>
-                                        <td><?php echo $row['jumlah'] ?></td>
+                                        <td><?php echo $row['jumlah_bpjs'] ?></td>
+                                        <td><?php echo $row['jumlah_non_bpjs'] ?></td>
                                         <td><?php echo $row['nmsatker'] ?></td>
                                         <td><?php echo $row['updated_at'] ?></td>
                                     </tr>
