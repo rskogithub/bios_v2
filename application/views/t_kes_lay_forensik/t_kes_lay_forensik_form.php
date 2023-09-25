7b1d604ebd03ced0108b68b1d70c06096b3a8f5d
khotamawb <55381715+khotamawb@users.noreply.github.com>
2023-09-25 09:21:47 +0700

    Update t_kes_lay_forensik_form.php

--- application/views/t_kes_lay_forensik/t_kes_lay_forensik_form.php
+++ application/views/t_kes_lay_forensik/t_kes_lay_forensik_form.php
@@ -3,7 +3,7 @@
         <div class="col-xl-12">
             <div id="panel-1" class="panel">
                 <div class="panel-hdr">
-                    <h2>KELOLA DATA LAYANAN FORENSIK</h2>
+                    <h2>KELOLA DATA LAYANAN FORENSIK 1</h2>
                     <div class="panel-toolbar">
                         <button class="btn btn-panel" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
                         <button class="btn btn-panel" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
